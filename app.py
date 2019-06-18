8395d5551408da52c10a8563f5c186be0a64d553
tohhongxiang123 <tohhongxiang@gmail.com>
2019-06-18 20:39:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ app = Flask(__name__)
 
 image_dir = "static/images"
 images = os.listdir(image_dir)
+images_without_ext = [image.split(".")[0] for image in images]
 len_images = len(images)
 print(str(len(images)) + "images loaded")
 
 @app.route("/")
-def home():
+@app.route("/carousel")
+def carousel():
 	shuffle(images)
-	return render_template("index.html", images=images, len_images=len_images)
+	return render_template("pic_gallery.html", images=images, len_images=len_images, title="Carousel")
 
 @app.route("/list")
 def list():
 	shuffle(images)
-	return render_template("index.html", images=images, len_images=len_images)
+	return render_template("list.html", images=images, len_images=len_images, images_without_ext=images_without_ext, title="List")
 
 if __name__ == "__main__":
 	app.run(debug=True)
