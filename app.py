3965c5be338f4359f6d98cb15cad6d810d16ea8c
tohhongxiang123 <tohhongxiang@gmail.com>
2019-06-20 23:13:02 +0800

    Add files via upload
    This is why you pull before committing

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ app = Flask(__name__)
 
 image_dir = "static/images"
 images = [image for image in os.listdir(image_dir) if image.split(".")[-1].lower() in ["jpg", "png", "jpeg", "bmp", "gif"]]
+print(images)
 len_images = len(images)
 print(str(len(images)) + "images loaded")
 
 @app.route("/")
-def home():
+@app.route("/carousel")
+def carousel():
 	shuffle(images)
-	return render_template("index.html", images=images, len_images=len_images)
+	return render_template("pic_gallery.html", images=images, len_images=len_images)
 
 @app.route("/list")
 def list():
 	shuffle(images)
-	return render_template("index.html", images=images, len_images=len_images)
+	return render_template("list.html", images=images, len_images=len_images)
 
 if __name__ == "__main__":
 	app.run(debug=True)
