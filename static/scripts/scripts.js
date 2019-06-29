84c7b0c6c481d00d85d3c1e718747b862f541edb
tohhongxiang123 <tohhongxiang@gmail.com>
2019-06-29 20:37:17 +0800

    Add files via upload
    Changed album viewer layout to be nicer ish
    Added a back button to navigate from viewing a single album
    Added lazy loading for image viewing carousel

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -10,3 +10,12 @@ if (e.keyCode === 39) { // if right arrow
    return false;
 }
 });
+
+$(function() {
+	return $(".carousel.lazy").on("slide.bs.carousel", function(e){
+		var lazy;
+		lazy = $(e.relatedTarget).find("img[data-src]");
+		lazy.attr("src", lazy.data("src"));
+		lazy.removeAttr("data-src");
+	});
+});
